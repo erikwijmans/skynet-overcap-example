94e65844fc8f888612fc3ac349972f3f67c6ad9d
Erik Wijmans <etw@gatech.edu>
2019-09-22 10:32:31 -0400

    Update overcap_batch.sh

--- overcap_batch.sh
+++ overcap_batch.sh
@@ -8,7 +8,7 @@
 #SBATCH --qos=overcap
 
 # "--signal=USR1@600" sends a signal to the job _step_ when it needs to exit.  
-# It has 10 minutes to do so, otherwise it is forcability killed
+# It has 10 minutes to do so, otherwise it is forcibly killed
 
 # This srun is critical!  The signal won't be sent correctly otherwise
 srun python -u -m overcap_example.train_cifar10
