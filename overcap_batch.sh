5275381054500bf05bf243e428ce362675ce0729
Erik Wijmans <etw@gatech.edu>
2020-08-21 12:03:12 -0400

    Update overcap_batch.sh

--- overcap_batch.sh
+++ overcap_batch.sh
@@ -6,7 +6,7 @@
 #SBATCH --partition=short
 #SBATCH --signal=USR1@300
 #SBATCH --requeue
-#SBATCH --acount=overcap
+#SBATCH --account=overcap
 
 # "--signal=USR1@300" sends a signal to the job _step_ when it needs to exit.
 # It has 5 minutes to do so, otherwise it is forcibly killed
